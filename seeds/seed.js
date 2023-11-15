01cfb335a42099a0d6dc00cfb16468f4f40cdc39
stevendreed <steven.daniel.reed@gmail.com>
2023-11-14 18:49:31 -0800

    swapped seeding from project to post

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,21 +1,24 @@
 const sequelize = require('../config/connection');
-const { User, Project } = require('../models');
+const { User, Post } = require('../models');
 
 const userData = require('./userData.json');
-const projectData = require('./projectData.json');
+const postData = require('./postData.json');
 
 const seedDatabase = async () => {
-  await sequelize.sync({ force: true });
+  await sequelize.sync(
+    { force: true }
+    );
 
   const users = await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
 
-  for (const project of projectData) {
-    await Project.create({
-      ...project,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
+  for (const post of postData) {
+    await Post.create({
+      ...post,
+      user_id: users[Math.floor(
+        Math.random() * users.length)].id,
     });
   }
 
